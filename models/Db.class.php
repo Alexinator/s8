aba88dbf5e17e203f4b11419ed671e5c7539c002
Alexandre Chapelle <alexchap98@hotmail.com>
2020-01-01 18:44:20 +0100

    s

--- models/Db.class.php
+++ models/Db.class.php
@@ -7,6 +7,7 @@ class Db
     private function __construct()
     {
         try {
+            
             $db = parse_url(getenv("postgres://bmtmuxnraprspi:497b6089f9579eda567c2e63aea1fce147a026f3e1a9359837f71181137760ca@ec2-46-137-188-105.eu-west-1.compute.amazonaws.com:5432/d8rr751nf80vnc"));
             $this->_db = new PDO("pgsql:" . sprintf(
                 "host=%s;port=%s;user=%s;password=%s;dbname=%s",
