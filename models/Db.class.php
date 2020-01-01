fa8ecc42df7a772df4628f5fbc2d266c76a9b6ed
Alexandre Chapelle <alexchap98@hotmail.com>
2020-01-01 18:40:05 +0100

    s

--- models/Db.class.php
+++ models/Db.class.php
@@ -18,7 +18,6 @@ class Db
             ));
             $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
             $this->_db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-            die('ok: connecté ');
         } 
 		catch (PDOException $e) {
 		    die('Erreur de connexion à la base de données ici: '.$e->getMessage());
